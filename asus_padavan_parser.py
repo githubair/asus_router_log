0a6625b8135adcb324e5220191fd925b649e1d3b
Roman Prokuda <hotrem@gmail.com>
2019-04-17 14:17:23 +0200

    IP log parser
    find duplicate IP

---
+++ asus_padavan_parser.py
@@ -0,0 +1,31 @@
+import re
+from collections import Counter
+
+
+def reader(filename):
+    regexp = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+
+    with open(filename) as f:
+        log = f.readlines()
+        a = [x.split() for x in log]  # построчно загоняю в лист
+        # print (a)
+        ips_list = [i[1] for i in a]  # записываю в ips_list только перв. столбец IP
+        # print (ips_list)
+        new_list = []
+        for i in ips_list:
+            ipf = re.findall(regexp, i)
+            if ipf in new_list:
+                continue
+            elif len(ipf) > 0:
+                new_list.append(ipf[0])
+        # print (new_list)
+        count = Counter (new_list)
+        # print (count)
+        for i in count:
+        	print (i, '\t', count[i])
+
+
+
+
+if __name__ == '__main__':
+    reader('69mb3.txt')
